70a5ae07e996d61a05c897604ca3f58af2230345
0811setabakovic <0811setabakovic@student.epfc.eu>
2023-09-26 13:46:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# x = int(input("entrez une valeur entière:"))
+# print(x)
+# c1 = "L'information de la chaîne"
+# c2 = 'avec des "apostrophes" !!'
+# print(c1, c2)
+#
+# p1 = 'une chaine \nen deux lignes'
+# print(len(p1))
+
+# r1 = r'une chaine \nen mode raw'
+# print(r1)
+#
+# c4 = "help!"
+# c5 = c4 * 4
+# print(c5)
+#
+# c6 = "une ligne à découper"
+# print(c6.split(' '))
+
+# print('-'.join(['c\'est', 'à', 'dire']))
+
+# print('this is a title'.title())
+
+# print('fRANK'.swapcase())
+
+# print("    Trop d'espaces    ".strip(' '))
+
+# print('abracadabra'.replace('a', 'i'))
+
+# c1 = 'abcdefghijklmnop'
+# print(c1[5])
+# print(c1[-1])
+# print(c1[1:5])
+# print(c1[1:])
+# print(c1[:5])
+#
+# x = 25
+# print("%d en base 8: %o" % (x,x))
+# print("%d en base 8: %x" % (x,x))
